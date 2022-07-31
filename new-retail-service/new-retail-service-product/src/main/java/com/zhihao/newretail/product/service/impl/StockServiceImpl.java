f41e9ed82ce57ee1933210222e43e0225c9476cc
Zhihao <CAFEBABE0508@163.com>
2022-08-01 02:16:10 +0800

    refactor(ProductStockFeignService):remove ServiceException

--- new-retail-service/new-retail-service-product/src/main/java/com/zhihao/newretail/product/service/impl/StockServiceImpl.java
+++ new-retail-service/new-retail-service-product/src/main/java/com/zhihao/newretail/product/service/impl/StockServiceImpl.java
@@ -9,12 +9,10 @@ import com.zhihao.newretail.product.enums.SkuStockLockEnum;
 import com.zhihao.newretail.product.pojo.SkuStock;
 import com.zhihao.newretail.product.pojo.SkuStockLock;
 import com.zhihao.newretail.product.service.StockService;
-import org.apache.http.HttpStatus;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.CollectionUtils;
 
 import java.util.List;
 import java.util.Set;
@@ -37,16 +35,12 @@ public class StockServiceImpl implements StockService {
     @Override
     public List<SkuStockApiVO> listSkuStockApiVOs(Set<Integer> skuIdSet) {
         List<SkuStock> skuStockList = skuStockMapper.selectListBySkuIdSet(skuIdSet);
-
-        if (!CollectionUtils.isEmpty(skuStockList)) {
-            return skuStockList.stream()
-                    .map(skuStock -> {
-                        SkuStockApiVO skuStockApiVO = new SkuStockApiVO();
-                        BeanUtils.copyProperties(skuStock, skuStockApiVO);
-                        return skuStockApiVO;
-                    }).collect(Collectors.toList());
-        }
-        throw new ServiceException(HttpStatus.SC_NOT_FOUND, "商品库存信息异常");
+        return skuStockList.stream()
+                .map(skuStock -> {
+                    SkuStockApiVO skuStockApiVO = new SkuStockApiVO();
+                    BeanUtils.copyProperties(skuStock, skuStockApiVO);
+                    return skuStockApiVO;
+                }).collect(Collectors.toList());
     }
 
     @Override

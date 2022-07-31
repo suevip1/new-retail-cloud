359086eaa8ba24fd7326732e56e8ee82fceb8a0e
Zhihao <CAFEBABE0508@163.com>
2022-08-01 01:48:15 +0800

    refactor(UserCouponsFeignService):remove ServiceException

--- new-retail-service/new-retail-service-user/src/main/java/com/zhihao/newretail/user/service/impl/UserCouponsServiceImpl.java
+++ new-retail-service/new-retail-service-user/src/main/java/com/zhihao/newretail/user/service/impl/UserCouponsServiceImpl.java
@@ -4,15 +4,12 @@ import com.zhihao.newretail.api.coupons.dto.CouponsBatchApiDTO;
 import com.zhihao.newretail.api.coupons.feign.CouponsFeignService;
 import com.zhihao.newretail.api.coupons.vo.CouponsApiVO;
 import com.zhihao.newretail.api.user.vo.UserCouponsApiVO;
-import com.zhihao.newretail.core.exception.ServiceException;
 import com.zhihao.newretail.user.dao.UserCouponsMapper;
 import com.zhihao.newretail.user.pojo.UserCoupons;
 import com.zhihao.newretail.user.service.UserCouponsService;
-import org.apache.http.HttpStatus;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.util.CollectionUtils;
 
 import java.util.List;
 import java.util.Set;
@@ -30,18 +27,10 @@ public class UserCouponsServiceImpl implements UserCouponsService {
     @Override
     public List<CouponsApiVO> listUserCouponsVOs(Integer userId) {
         List<UserCoupons> userCouponsList = userCouponsMapper.selectListByUserId(userId);
-
-        if (CollectionUtils.isEmpty(userCouponsList))
-            throw new ServiceException(HttpStatus.SC_NO_CONTENT, "暂无数据");
-
         Set<Integer> couponsIdSet = userCouponsList.stream().map(UserCoupons::getCouponsId).collect(Collectors.toSet());
         CouponsBatchApiDTO couponsBatchApiDTO = new CouponsBatchApiDTO();
         couponsBatchApiDTO.setCouponsIdSet(couponsIdSet);
-        List<CouponsApiVO> couponsApiVOList = couponsFeignService.listCouponsApiVOs(couponsBatchApiDTO);
-
-        if (CollectionUtils.isEmpty(couponsApiVOList))
-            throw new ServiceException(HttpStatus.SC_NO_CONTENT, "暂无数据");
-        return couponsApiVOList;
+        return couponsFeignService.listCouponsApiVOs(couponsBatchApiDTO);
     }
 
     @Override
@@ -52,7 +41,6 @@ public class UserCouponsServiceImpl implements UserCouponsService {
     @Override
     public List<UserCouponsApiVO> listUserCouponsApiVOs(Integer userId) {
         List<UserCoupons> userCouponsList = userCouponsMapper.selectListByUserId(userId);
-
         return userCouponsList.stream()
                 .map(userCoupons -> {
                     UserCouponsApiVO userCouponsApiVO = new UserCouponsApiVO();

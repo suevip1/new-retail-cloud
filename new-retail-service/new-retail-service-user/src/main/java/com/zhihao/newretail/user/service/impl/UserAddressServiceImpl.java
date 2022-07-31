b376f725f06af9f1eb6bbfe69816f7ccd8b4988c
Zhihao <CAFEBABE0508@163.com>
2022-08-01 02:16:24 +0800

    refactor(UserAddressFeignService):remove ServiceException

--- new-retail-service/new-retail-service-user/src/main/java/com/zhihao/newretail/user/service/impl/UserAddressServiceImpl.java
+++ new-retail-service/new-retail-service-user/src/main/java/com/zhihao/newretail/user/service/impl/UserAddressServiceImpl.java
@@ -13,7 +13,6 @@ import org.apache.http.HttpStatus;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.util.CollectionUtils;
 import org.springframework.util.ObjectUtils;
 
 import java.util.List;

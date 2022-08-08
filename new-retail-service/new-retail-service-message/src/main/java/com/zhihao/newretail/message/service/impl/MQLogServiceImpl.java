ab17855a186e79b42676e000150abecc1d6afac1
Zhihao <CAFEBABE0508@163.com>
2022-08-09 02:41:06 +0800

    feat(MQLogService):MQLog CRUD

--- new-retail-service/new-retail-service-message/src/main/java/com/zhihao/newretail/message/service/impl/MQLogServiceImpl.java
+++ new-retail-service/new-retail-service-message/src/main/java/com/zhihao/newretail/message/service/impl/MQLogServiceImpl.java
@@ -1,7 +1,9 @@
 package com.zhihao.newretail.message.service.impl;
 
+import com.zhihao.newretail.message.dao.MQLogMapper;
 import com.zhihao.newretail.message.pojo.MQLog;
 import com.zhihao.newretail.message.service.MQLogService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /*
@@ -12,24 +14,27 @@ import org.springframework.stereotype.Service;
 @Service
 public class MQLogServiceImpl implements MQLogService {
 
+    @Autowired
+    private MQLogMapper mqLogMapper;
+
     @Override
     public void insetMessage(MQLog mqLog) {
-
+        mqLogMapper.insertSelective(mqLog);
     }
 
     @Override
     public void updateMessage(MQLog mqLog) {
-
+        mqLogMapper.updateByPrimaryKeySelective(mqLog);
     }
 
     @Override
     public void deleteMessage(Long messageId) {
-
+        mqLogMapper.deleteByPrimaryKey(messageId);
     }
 
     @Override
     public MQLog getMQLog(Long messageId) {
-        return null;
+        return mqLogMapper.selectByPrimaryKey(messageId);
     }
 
 }

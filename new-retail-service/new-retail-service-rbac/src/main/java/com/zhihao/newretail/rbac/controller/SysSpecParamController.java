9e6a0064ae278dfac82e6031c2c15666d08b595c
52CL <cafebabe0508@163.com>
2022-08-18 22:40:12 +0800

    fix(SysSpecParamController):requestMapping

--- new-retail-service/new-retail-service-rbac/src/main/java/com/zhihao/newretail/rbac/controller/SysSpecParamController.java
+++ new-retail-service/new-retail-service-rbac/src/main/java/com/zhihao/newretail/rbac/controller/SysSpecParamController.java
@@ -9,11 +9,13 @@ import com.zhihao.newretail.security.context.UserLoginContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/admin")
 public class SysSpecParamController {
 
     @Autowired

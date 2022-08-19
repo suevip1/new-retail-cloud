020b69fe72dad7a0dcee8d34c5e319f334fe660d
52CL <cafebabe0508@163.com>
2022-08-19 23:09:40 +0800

    feat(RbacService):upload test

--- new-retail-service/new-retail-service-file/src/main/java/com/zhihao/newretail/file/feign/FileUploadFeignController.java
+++ new-retail-service/new-retail-service-file/src/main/java/com/zhihao/newretail/file/feign/FileUploadFeignController.java
@@ -2,14 +2,12 @@ package com.zhihao.newretail.file.feign;
 
 import com.zhihao.newretail.api.file.feign.FileUploadFeignService;
 import com.zhihao.newretail.file.service.FileService;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
-@Slf4j
 @RestController
 public class FileUploadFeignController implements FileUploadFeignService {
 
@@ -18,8 +16,6 @@ public class FileUploadFeignController implements FileUploadFeignService {
 
     @Override
     public String getFileUrl(MultipartFile file, String dir) throws IOException {
-        log.info("fileName: {}", file.getOriginalFilename());
-        log.info("dir: {}", dir);
         return fileService.upload(file, dir);
     }
 

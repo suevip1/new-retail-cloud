edb5cff3c552eb0e661adbc7259872cdef91ea15
52CL <cafebabe0508@163.com>
2022-08-22 23:39:00 +0800

    feat ProductSentinelConfig

---
+++ new-retail-service/new-retail-service-product/src/main/java/com/zhihao/newretail/product/config/ProductSentinelConfig.java
@@ -0,0 +1,26 @@
+package com.zhihao.newretail.product.config;
+
+import com.alibaba.csp.sentinel.adapter.spring.webmvc.callback.BlockExceptionHandler;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.zhihao.newretail.core.util.GsonUtil;
+import com.zhihao.newretail.core.util.R;
+import org.apache.http.HttpStatus;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@Configuration
+public class ProductSentinelConfig implements BlockExceptionHandler {
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, BlockException e) throws Exception {
+        R error = R.error(HttpStatus.SC_SERVICE_UNAVAILABLE, "当前访问人数过多，请稍后重试");
+        response.setHeader("Content-Type","application/json;charset=UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json");
+        response.getWriter().write(GsonUtil.obj2Json(error));
+    }
+
+}

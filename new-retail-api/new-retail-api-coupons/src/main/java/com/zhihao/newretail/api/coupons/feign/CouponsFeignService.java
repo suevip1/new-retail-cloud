2652b176b9fd09d4243cebe9be1150c1bf6077aa
52CL <cafebabe0508@163.com>
2022-08-25 01:05:30 +0800

    feat CouponsFeignFallback

--- new-retail-api/new-retail-api-coupons/src/main/java/com/zhihao/newretail/api/coupons/feign/CouponsFeignService.java
+++ new-retail-api/new-retail-api-coupons/src/main/java/com/zhihao/newretail/api/coupons/feign/CouponsFeignService.java
@@ -1,22 +1,25 @@
 package com.zhihao.newretail.api.coupons.feign;
 
-import com.zhihao.newretail.api.coupons.dto.CouponsBatchApiDTO;
+import com.zhihao.newretail.api.coupons.fallback.CouponsFeignFallback;
 import com.zhihao.newretail.api.coupons.vo.CouponsApiVO;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.Primary;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
+import java.util.Set;
 
-@FeignClient(name = "new-retail-coupons", path = "/coupons")
+@Primary
+@FeignClient(name = "new-retail-coupons", path = "/coupons", fallback = CouponsFeignFallback.class)
 public interface CouponsFeignService {
 
     @GetMapping("/api/coupons/{couponsId}")
     CouponsApiVO getCouponsApiVO(@PathVariable Integer couponsId);
 
     @PostMapping("/api/coupons/list")
-    List<CouponsApiVO> listCouponsApiVOs(@RequestBody CouponsBatchApiDTO couponsBatchApiDTO);
+    List<CouponsApiVO> listCouponsApiVOS(@RequestBody Set<Integer> couponsIdSet);
 
 }

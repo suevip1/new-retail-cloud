2bcbde6472ba0c960035302c8130c8505d2202a4
52CL <cafebabe0508@163.com>
2022-08-25 21:27:01 +0800

    feat PageUtil

---
+++ new-retail-common/new-retail-common-core/src/main/java/com/zhihao/newretail/core/util/PageUtil.java
@@ -0,0 +1,54 @@
+package com.zhihao.newretail.core.util;
+
+import java.util.List;
+
+public class PageUtil<T> {
+
+    private Integer pageNum;
+    private Integer pageSize;
+    private Long total;
+    private List<T> list;
+
+    public Integer getPageNum() {
+        return pageNum;
+    }
+
+    public void setPageNum(Integer pageNum) {
+        this.pageNum = pageNum;
+    }
+
+    public Integer getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(Integer pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public Long getTotal() {
+        return total;
+    }
+
+    public void setTotal(Long total) {
+        this.total = total;
+    }
+
+    public List<T> getList() {
+        return list;
+    }
+
+    public void setList(List<T> list) {
+        this.list = list;
+    }
+
+    @Override
+    public String toString() {
+        return "PageUtil{" +
+                "pageNum=" + pageNum +
+                ", pageSize=" + pageSize +
+                ", total=" + total +
+                ", list=" + list +
+                '}';
+    }
+
+}

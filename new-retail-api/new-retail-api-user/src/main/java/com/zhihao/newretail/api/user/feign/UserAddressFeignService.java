ab402b2d31da86312b3ed2f76f60440d54e082b8
52CL <cafebabe0508@163.com>
2022-08-25 11:46:47 +0800

    fix(UserAddressFeignService):UserAddressFeignFallback

--- new-retail-api/new-retail-api-user/src/main/java/com/zhihao/newretail/api/user/feign/UserAddressFeignService.java
+++ new-retail-api/new-retail-api-user/src/main/java/com/zhihao/newretail/api/user/feign/UserAddressFeignService.java
@@ -1,5 +1,6 @@
 package com.zhihao.newretail.api.user.feign;
 
+import com.zhihao.newretail.api.user.fallback.UserAddressFeignFallback;
 import com.zhihao.newretail.api.user.vo.UserAddressApiVO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.context.annotation.Primary;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import java.util.List;
 
 @Primary
-@FeignClient(name = "new-retail-user", path = "/user")
+@FeignClient(name = "new-retail-user", path = "/user", fallback = UserAddressFeignFallback.class)
 public interface UserAddressFeignService {
 
     @GetMapping("/api/address/list")

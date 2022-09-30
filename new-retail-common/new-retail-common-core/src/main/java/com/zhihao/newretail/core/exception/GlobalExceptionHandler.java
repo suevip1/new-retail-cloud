7b6fa1ee6df7d94bd6a373232102f80f4bfe13d9
52CL <cafebabe0508@163.com>
2022-09-30 20:07:22 +0800

    opt(GlobalExceptionHandler):handlerCompletionException

--- new-retail-common/new-retail-common-core/src/main/java/com/zhihao/newretail/core/exception/GlobalExceptionHandler.java
+++ new-retail-common/new-retail-common-core/src/main/java/com/zhihao/newretail/core/exception/GlobalExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Objects;
+import java.util.concurrent.CompletionException;
 
 /*
  * 全局异常处理
@@ -24,7 +25,7 @@ public class GlobalExceptionHandler {
         String requestURI = request.getRequestURI();
         Integer code = e.getCode();
         String msg = e.getMsg();
-        log.error("请求地址'{}','{}'", requestURI, msg);
+        log.error("请求地址'{}',业务执行异常.message'{}'", requestURI, msg);
         return R.error(code, msg);
     }
 
@@ -35,7 +36,7 @@ public class GlobalExceptionHandler {
     public R handlerMethodArgumentNotValidException(MethodArgumentNotValidException e, HttpServletRequest request) {
         String requestURI = request.getRequestURI();
         String message = Objects.requireNonNull(e.getBindingResult().getFieldError()).getDefaultMessage();
-        log.error("请求地址'{}','{}'", requestURI, message);
+        log.error("请求地址'{}',表单校验异常.message'{}'", requestURI, message);
         return R.error(message);
     }
 
@@ -46,6 +47,16 @@ public class GlobalExceptionHandler {
     public R handlerRuntimeException(RuntimeException e, HttpServletRequest request) {
         String requestURI = request.getRequestURI();
         log.error("请求地址'{}',后端执行异常.", requestURI, e);
+        return R.error(e.getMessage());
+    }
+
+    /*
+    * CompletableFuture 异步编排异常
+    * */
+    @ExceptionHandler(CompletionException.class)
+    public R handlerCompletionException(CompletionException e, HttpServletRequest request) {
+        String requestURI = request.getRequestURI();
+        log.error("请求地址'{}',异步调用异常.message'{}'", requestURI, e.getMessage());
         return R.error(e.getMessage());
     }
 

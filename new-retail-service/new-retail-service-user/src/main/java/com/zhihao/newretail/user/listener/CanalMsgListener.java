19437b985dbe5cf885908f7842c94de1cfee0248
52CL <cafebabe0508@163.com>
2022-10-02 20:03:14 +0800

    fix(UserCanalMsgListener):jsonData is null

--- new-retail-service/new-retail-service-user/src/main/java/com/zhihao/newretail/user/listener/CanalMsgListener.java
+++ new-retail-service/new-retail-service-user/src/main/java/com/zhihao/newretail/user/listener/CanalMsgListener.java
@@ -9,10 +9,10 @@ import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.util.ObjectUtils;
 
 import java.io.IOException;
-import java.util.HashMap;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ThreadPoolExecutor;
 
 import static com.zhihao.newretail.user.consts.UserCacheKeyConst.USER_INFO;
 
@@ -23,28 +23,42 @@ public class CanalMsgListener {
     @Autowired
     private MyRedisUtil redisUtil;
 
+    @Autowired
+    private ThreadPoolExecutor executor;
+
     private static final Gson gson = new GsonBuilder().create();
 
     @RabbitListener(queues = RabbitMQConst.CANAL_USER_QUEUE)
     public void canalMsgQueue(String msgStr, Message message, Channel channel) throws IOException {
         log.info("用户服务，接收canal消息: {}", msgStr);
-        JsonObject jsonObject = gson.fromJson(msgStr, JsonObject.class);
-        JsonArray data = jsonObject.get("data").getAsJsonArray();
-        JsonArray old = jsonObject.get("old").getAsJsonArray();
-        for (JsonElement element : data) {
-            HashMap<String, Object> dataMap = gson.fromJson(element, HashMap.class);
-            Integer userId = Integer.valueOf(String.valueOf(dataMap.get("user_id")));
-            redisUtil.deleteObject(String.format(USER_INFO, userId));
-            /* 删除旧数据 */
-            for (JsonElement oldData : old) {
-                HashMap<String, Object> oldDataMap = gson.fromJson(oldData, HashMap.class);
-                /* 抽取关键字段，删除旧 id 数据 */
-                Object oldUserId = oldDataMap.get("user_id");
-                if (!ObjectUtils.isEmpty(oldUserId)) {
-                    redisUtil.deleteObject(String.format(USER_INFO, Integer.valueOf(String.valueOf(oldUserId))));
+        JsonObject json = gson.fromJson(msgStr, JsonObject.class);
+        CompletableFuture<Void> syncCurrentDataFuture = CompletableFuture.runAsync(() -> {
+            JsonElement data = json.get("data");
+            if (!data.isJsonNull()) {
+                JsonArray currentDataList = data.getAsJsonArray();
+                for (JsonElement jsonElement : currentDataList) {
+                    JsonObject currentData = jsonElement.getAsJsonObject();
+                    redisUtil.deleteObject(String.format(USER_INFO, currentData.get("user_id").getAsInt()));
+                    log.info("用户服务，当前数据处理完成");
                 }
+            } else {
+                log.info("用户服务，当前数据为空，无需处理");
             }
-        }
+        }, executor);
+        CompletableFuture<Void> syncOldDataFuture = CompletableFuture.runAsync(() -> {
+            JsonElement old = json.get("old");
+            if (!old.isJsonNull()) {
+                JsonArray oldDataList = old.getAsJsonArray();
+                for (JsonElement jsonElement : oldDataList) {
+                    JsonObject oldData = jsonElement.getAsJsonObject();
+                    redisUtil.deleteObject(String.format(USER_INFO, oldData.get("user_id").getAsInt()));
+                    log.info("用户服务，旧数据删除成功");
+                }
+            } else {
+                log.info("用户服务，旧数据为空，无需处理");
+            }
+        }, executor);
+        CompletableFuture.allOf(syncCurrentDataFuture, syncOldDataFuture).join();
         channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
     }
 

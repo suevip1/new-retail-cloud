d7b9862583b3f2760a530cc4796aa80d1b6d3303
52CL <cafebabe0508@163.com>
2022-11-12 01:10:08 +0800

    fix(SkuFeignService):import *

--- new-retail-api/new-retail-api-product/src/main/java/com/zhihao/newretail/api/product/feign/SkuFeignService.java
+++ new-retail-api/new-retail-api-product/src/main/java/com/zhihao/newretail/api/product/feign/SkuFeignService.java
@@ -5,7 +5,11 @@ import com.zhihao.newretail.api.product.dto.SkuUpdateApiDTO;
 import com.zhihao.newretail.api.product.fallback.SkuFeignFallback;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.context.annotation.Primary;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Primary
 @FeignClient(name = "new-retail-product", path = "/new-retail-product", fallback = SkuFeignFallback.class)
